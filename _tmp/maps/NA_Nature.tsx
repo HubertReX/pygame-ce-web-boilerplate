543dec84a09a079fada09ebb37e8bf4aa89d771b
Hubert Nafalski <hubert.nafalski@gmail.com>
2024-04-21 17:14:59 +0200

    cleanup temp

--- _tmp/maps/NA_Nature.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="NA_Nature" tilewidth="16" tileheight="16" tilecount="504" columns="24">
- <image source="Tilesets/TilesetNature.png" width="384" height="336"/>
-</tileset>
