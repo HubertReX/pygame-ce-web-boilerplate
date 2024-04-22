554ba4b90140dbacc9a25e81153559c8967f8233
Hubert Nafalski <hubert.nafalski@gmail.com>
2024-04-22 21:59:52 +0200

    improved build script; black loading screen

--- serve_web.sh
+++ serve_web.sh
@@ -1,5 +1,8 @@
 # blocking (when using sounds)
-# pygbag --ume_block 1 --icon project/assets/icon.png project
+# pygbag --ume_block 1 --template utils/default.tmpl --icon project/assets/icon.png --no_opt  project
 
-# non blocking - quicker (no sounds)
-pygbag --ume_block 0 --template utils/default.tmpl --icon project/assets/icon.png project
+# make it available from local network
+# pygbag --ume_block 0 --template utils/default.tmpl --icon project/assets/icon.png --no_opt --bind 0.0.0.0 project
+
+# non blocking - quicker (no sounds) --no_opt so the pixel art is not distorted with optimisation
+pygbag --ume_block 0 --template utils/default.tmpl --icon project/assets/icon.png --no_opt project
