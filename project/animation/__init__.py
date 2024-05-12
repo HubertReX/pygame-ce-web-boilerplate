097bf9d095e43ae7397c8304bf6769dc67312ae2
Hubert Nafalski <hubert.nafalski@gmail.com>
2024-05-12 22:07:45 +0200

    * added amazing lib that allows animate any numerical value (schedule, chain, delay) with many transitions allowing to create custom cutscenes * created workflow to create json with cutscene definition and automatic execution

---
+++ project/animation/__init__.py
@@ -0,0 +1,3 @@
+from .animation import Animation, Task, remove_animations_of, animator
+
+__version__ = '0.0.5'
--- utils/cutscene.py
+++ utils/cutscene.py
@@ -1,7 +1,14 @@
+from functools import partial
+from typing import Any, Callable
 import pygame
 import sys
+from animation import Animation, Task, remove_animations_of, animator
 
 # https://www.codewithc.com/advanced-cutscene-creation-in-pygame/
+#   plus
+# https://github.com/bitcraft/animation
+
+# just for testing, move to ../project folder before running
 
 # Initialize Pygame
 pygame.init()
@@ -46,6 +53,8 @@ class CutsceneManager:
                     self.active_scene = None
             else:
                 self.active_scene.update()
+        
+        return bool(self.active_scene)
 
     def draw(self, screen):
         if self.active_scene is not None:
@@ -69,11 +78,29 @@ class Scene:
 class DialogueScene(Scene):
     def __init__(self, text, duration):
         self.text = text
+        self.r = 255
+        self.g = 0
+        self.b = 0
+        self.color = (self.r, self.g, self.b)
         self.duration = duration
-        self.font = pygame.font.SysFont('Arial', 32)
+        self.font_size = 32
+        self.font = pygame.font.SysFont('Arial', self.font_size)
         self.start_ticks = pygame.time.get_ticks()
         self.elapsed_time = 0
+        self.render_text = self.font.render(self.text, True, self.color)
+        self.text_rect = self.render_text.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
 
+    def change_font(self, _min, _max):
+        if self.font_size == _min:
+            self.font_size = _max
+        else:
+            self.font_size = _min
+        self.font = pygame.font.SysFont('Arial', self.font_size)
+        
+    def set_color(self, r, g=0, b=0):
+        self.color = (self.r, self.g, self.b)
+        # self.color = [r, g, b]
+        
     def start(self):
         self.start_ticks = pygame.time.get_ticks()
 
@@ -83,9 +110,9 @@ class DialogueScene(Scene):
 
     def draw(self, screen):
         screen.fill(BLACK)
-        render_text = self.font.render(self.text, True, WHITE)
-        text_rect = render_text.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
-        screen.blit(render_text, text_rect)
+        self.color = (self.r, self.g, self.b)
+        self.render_text = self.font.render(self.text, True, self.color)
+        screen.blit(self.render_text, self.text_rect)
 
     def is_finished(self):
         return self.elapsed_time >= self.duration
@@ -98,19 +125,116 @@ cutscene_manager.add_scene(DialogueScene('The maid screamed.', 3))
 cutscene_manager.add_scene(DialogueScene('And all was silent...', 5))
 cutscene_manager.start()
 
+
+# a1 = Animation(cutscene_manager.active_scene.text_rect, x=700, y=500, duration=1000, transition="in_out_quint")
+# a2 = Animation(cutscene_manager.active_scene, r=150, g=250, b=200, duration=2000)
+
+intro_cutscene = {
+    "steps": [
+        {
+            "name": "step_01",
+            "description": "move text to upper left corner",
+            "type": "animation",
+            "target": cutscene_manager.active_scene.text_rect, 
+            "args": {"x":10,  "y":10}, 
+            "duration": 1000, 
+            "transition": "in_out_cubic", 
+            "from": "<root>", 
+            "trigger": "<begin>"
+        },
+        {
+            "name": "step_05",
+            "description": "change font size",
+            "type": "task",
+            "target": cutscene_manager.active_scene.change_font, 
+            "args": {"_min": 64, "_max": 32}, 
+            "interval": 1000, 
+            "times": 2, 
+            "from": "step_02", 
+            # "trigger": "on finish"
+        },
+        {
+            "name": "step_02",
+            "description": "move text to lower right corner",
+            "type": "animation",
+            "target": cutscene_manager.active_scene.text_rect, 
+            "args": {"x":700,  "y":500}, 
+            "duration": 1000, 
+            "transition": "in_out_cubic", 
+            "from": "step_01", 
+            "trigger": "on finish"
+        },
+        {
+            "name": "step_03",
+            "description": "change font color from red to blue",
+            "type": "animation",
+            "target": cutscene_manager.active_scene, 
+            "args": {"r":50,  "g":250, "b":200}, 
+            "duration": 3000, 
+            # "transition": "in_out_cubic", 
+            "from": "step_01", 
+            "trigger": "on start"
+        },
+        {
+            "name": "step_04",
+            "type": "animation",
+            "description": "move text to lower left corner",
+            "target": cutscene_manager.active_scene.text_rect, 
+            "args": {"x":70,  "y":500}, 
+            "duration": 1000, 
+            "transition": "in_out_cubic", 
+            "from": "step_02", 
+            "trigger": "on finish"
+        },
+    ]
+}
+
+# a1  = Animation(x=10,  y=10,        duration=1000, transition="in_out_cubic")
+# a2  = Animation(x=700, y=500,       duration=1000, transition="in_out_quint")
+# a3  = Animation(r=50, g=250, b=200, duration=3000)
+# a4  = Animation(x=70, y=500,        duration=1000, transition="in_out_quint")
+
+# a1.schedule(partial(a3.start, cutscene_manager.active_scene), "on start")
+# a1.schedule(partial(a2.start, cutscene_manager.active_scene.text_rect))
+# a2.schedule(partial(a4.start, cutscene_manager.active_scene.text_rect))
+# a1.start(cutscene_manager.active_scene.text_rect)
+
+animations = pygame.sprite.Group()
+# animations.add([a1, a2, a3, a4])
+first_anim = animator(intro_cutscene, animations)
+
+
+# animations.add(a2)
+# animations.add(a3)
+
+# a2.start(cutscene_manager.active_scene)
+# animations.add(ani2)
+# animations.add(ani3)
 # Game loop
 running = True
 while running:
+    dt = clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+                running = False
+                # pygame.quit()
+                # sys.exit()
+                
+            elif event.key == pygame.K_SPACE:
+                if cutscene_manager.active_scene:
+                    cutscene_manager.active_scene.duration = -1
             
     screen.fill(BLACK)
-    cutscene_manager.update()
+                
+    if running:
+        running = cutscene_manager.update()
+    animations.update(dt)
     cutscene_manager.draw(screen)
     
     pygame.display.flip()
-    clock.tick(FPS)
 
 pygame.quit()
 sys.exit()
