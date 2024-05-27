bbd39208c6219e02b2556a0abb270818abcabe46
Hubert Nafalski <hubert.nafalski@gmail.com>
2024-05-27 23:15:42 +0200

    fixed fields step cost

--- project/assets/NinjaAdventure/maps/tilesets/Field.tsx
+++ project/assets/NinjaAdventure/maps/tilesets/Field.tsx
@@ -1,4 +1,229 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="NA_Field" tilewidth="16" tileheight="16" tilecount="75" columns="5">
  <image source="images/TilesetField.png" width="80" height="240"/>
+ <tile id="0">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="step_cost" type="int" value="200"/>
+  </properties>
+ </tile>
 </tileset>
