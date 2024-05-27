7a590fca155d0dcd8582ac5edd8c8b590009085c
Hubert Nafalski <hubert.nafalski@gmail.com>
2024-05-27 23:15:21 +0200

    screen recorder added directly since needs a patch

---
+++ project/pygame_screen_record/__init__.py
@@ -0,0 +1,4 @@
+from .EventRegister import EventRegister
+from .ScreenRecorder import (NPZPlayer, Recording, RecordingPlayer,
+                             RecordingSaver, ScreenRecorder, add_codec,
+                             available_formats)
